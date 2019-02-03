6bd8e820da29c7c599acbf02326303be01407793
simoncropp <simon.cropp@gmail.com>
2019-02-03 11:16:03 +1100

    init

---
+++ src/ApprovalTests.StatePrinter.Tests/StatePrinterTests.cs
@@ -0,0 +1,25 @@
+﻿using ApprovalTests.StatePrinter;
+using Xunit;
+
+public class StatePrinterTests
+{
+    [Fact]
+    public void TestVerifyCircleReferences()
+    {
+        var turtle = new Turtle("jim", new Turtle("kasper", new Turtle("llewellyn", null)));
+        turtle.On.On.On = turtle;
+        StatePrinterApprovals.Verify(turtle);
+    }
+}
+
+public class Turtle
+{
+    public string Name { get; set; }
+    public Turtle On { get; set; }
+
+    public Turtle(string name, Turtle onTurtle)
+    {
+        Name = name;
+        On = onTurtle;
+    }
+}
